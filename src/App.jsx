cf5fc1740c906c1e6922b74d5dc03742e84df48b
I Made Sudarsana Taksa Wibawa <taksawibawa@gmail.com>
2023-12-30 17:07:50 +0800

    add(pages-routes): add routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { Navbar } from "./components/navbar";
 import { Routes as BaseRouter, Route } from "react-router-dom";
+import { LoginPage, NotFoundPage, ProductDashboardPage, ProductDetailPage } from "./pages";
+import { PrivateRoute, ProtectedRoute } from "./routes";
 
 function App() {
 	return (
-		<BaseRouter>
-			<Route
-				path="/"
-				element={<h1>Home</h1>}
-			/>
-		</BaseRouter>
+		<>
+			<Navbar />
+			<BaseRouter>
+				<Route element={<ProtectedRoute />}>
+					<Route
+						path="/"
+						element={<LoginPage />}
+					/>
+				</Route>
+				<Route element={<PrivateRoute />}>
+					<Route
+						path="/product-dashboard"
+						element={<ProductDashboardPage />}
+					/>
+					<Route
+						path="/product-dashboard/:id"
+						element={<ProductDetailPage />}
+					/>
+				</Route>
+				<Route
+					path="*"
+					element={<NotFoundPage />}
+				/>
+			</BaseRouter>
+		</>
 	);
 }
 
